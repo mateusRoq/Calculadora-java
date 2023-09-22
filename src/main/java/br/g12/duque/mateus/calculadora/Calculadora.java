faef67fca3d2a637df730a3e91134fb1d355c859
Mateus Roque <mateus.roque@duque.g12.br>
2023-09-21 21:12:35 -0300

    java calculadora

---
+++ src/main/java/br/g12/duque/mateus/calculadora/Calculadora.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.g12.duque.mateus.calculadora;
+
+import java.awt.Color;
+
+/**
+ *
+ * @author 08060
+ */
+public class Calculadora {
+    public static void main(String[] args) {
+        FrmCalculadora calc = new FrmCalculadora();
+        calc.setLocationRelativeTo(calc);
+        calc.setBackground(Color.yellow);
+        calc.setVisible(true);
+    }
+}
